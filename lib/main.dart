64d11638bccb7a13dac15a9182498c36002ea11f
Ivy Walobwa <ivywalobwa@gmail.com>
2020-06-23 22:52:28 +0300

    conditionals

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [
+  ];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  int questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +75,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionIndex];
+                if(correctAnswer == true){
+                  print('you fot it right');
+                }else{
+                  print('you got it wrong');
+                }
+                setState(() {
+                  questionIndex++;
+                });
                 //The user picked true.
               },
             ),
@@ -79,12 +102,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionIndex];
+                if(correctAnswer == false){
+                  print('you fot it right');
+                }else{
+                  print('you got it wrong');
+                }
+                setState(() {
+                  questionIndex++;
+                });
                 //The user picked false.
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
